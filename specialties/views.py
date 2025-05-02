af4ec461d14d5560864cbf1d5fb0fbf538e1aabe
unknown <rejohn.cse@gmail.com>
2025-05-02 17:02:27 +0600

    Add Error handeling

--- specialties/views.py
+++ specialties/views.py
@@ -20,18 +20,23 @@ class SpecialtyViewSet(viewsets.ModelViewSet):
         return queryset
     
     def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset())
-        page = self.paginate_queryset(queryset)
-        
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
-        
-        serializer = self.get_serializer(queryset, many=True)
-        return custom_response(
-            message="All specialties retrieved successfully",
-            data=serializer.data
-        )
+        try:
+            queryset = self.filter_queryset(self.get_queryset())
+            page = self.paginate_queryset(queryset)
+            if page is not None:
+                serializer = self.get_serializer(page, many=True)
+                return self.get_paginated_response(serializer.data)
+            serializer = self.get_serializer(queryset, many=True)
+            return custom_response(
+                message="All specialties retrieved successfully",
+                data=serializer.data
+            )
+        except Exception as e:
+            return custom_response(
+                message=f"Error retrieving specialties: {str(e)}",
+                data=[],
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
 
     # def retrieve(self, request, *args, **kwargs):
     #     instance = self.get_object()
