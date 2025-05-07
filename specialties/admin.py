e8d807f78101384ac03c411c88541a4aaf16bde1
unknown <rejohn.cse@gmail.com>
2025-05-08 01:44:49 +0600

    Show addition info at list of specialties

--- specialties/admin.py
+++ specialties/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 from .models import Specialty
 
 # Register your models here.
-admin.site.register(Specialty)
+@admin.register(Specialty)
+class SpecialtyAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'is_published_display', 'created_at')
+    list_filter = ('is_published',)
+
+    @admin.display(boolean=True, description='Published')
+    def is_published_display(self, obj):
+        return obj.is_published
