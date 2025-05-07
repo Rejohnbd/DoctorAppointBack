4ef9e9c261b83ed6497f8a238181a519b9a2a0db
unknown <rejohn.cse@gmail.com>
2025-05-08 01:52:14 +0600

    Show show active & inactive distict, division, thana

--- location/admin.py
+++ location/admin.py
@@ -2,6 +2,29 @@ from django.contrib import admin
 from .models import Division, District, Thana
 
 # Register your models here.
-admin.site.register(Division)
-admin.site.register(District)
-admin.site.register(Thana)
+@admin.register(Division)
+class DivisionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'bn_name', 'is_published_display', 'created_at')
+    list_filter = ('is_published',)
+
+    @admin.display(boolean=True, description='Published')
+    def is_published_display(self, obj):
+        return obj.is_published
+
+@admin.register(District)
+class DistrictAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'bn_name', 'is_published_display', 'created_at')
+    list_filter = ('is_published',)
+
+    @admin.display(boolean=True, description='Published')
+    def is_published_display(self, obj):
+        return obj.is_published
+
+@admin.register(Thana)
+class ThanaAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'bn_name', 'is_published_display', 'created_at')
+    list_filter = ('is_published',)
+
+    @admin.display(boolean=True, description='Published')
+    def is_published_display(self, obj):
+        return obj.is_published
